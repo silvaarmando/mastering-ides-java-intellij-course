3182122cdfcee0b9bc1729232ff5d29b6674e07a
Armando Silva <franciscosilva7877@gmail.com>
2022-03-15 16:27:39 -0300

    "Add first commit"

---
+++ src/br/com/dio/FirstApplication.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import br.com.dio.model.Cat;
+
+public class FirstApplication {
+  public static void main(String[] args) {
+    Cat cat = new Cat();
+
+    System.out.println(cat);
+  }
+}
